9a7afc5395564651741d4ff57d83cc479b21ea8f
Matthew Conquergood <matthew.conquergood@gmail.com>
2015-03-14 18:16:38 -0700

    Database Connections Set Up

--- tasty/database.py
+++ tasty/database.py
@@ -1,1 +1,15 @@
 __author__ = 'kristjin@github'
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+from tasty import app
+
+# Create the engine conx to db at URI specified in config.py
+engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
+# Create a declarative base
+Base = declarative_base()
+# Start the session
+Session = sessionmaker(bind=engine)
+session = Session()
